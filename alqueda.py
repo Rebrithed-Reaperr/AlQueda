ca588c43b005a086349bd509a881aa22db88d552
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 17:09:01 -0500

    Update alqueda.py

--- alqueda.py
+++ alqueda.py
@@ -1,7 +1,7 @@
 import os, sys, time
 from time import sleep as timeout
 def restart_program():
-os.system("clear")
+os.system(' clear ')
 os.system("figlet Al Queda")
 print  "Created By: Rebrithed-Reaperr"
 print  "Youtube	  : BiggyGh0st"
@@ -9,11 +9,11 @@ print  "Github    : https://github.com/Rebrithed-Reaperr"
 print  "Instagram : @pwn.doxgod"
 print
 print  "									[1] DDos Attack 							"
-print  "									[2] Https/Http Attack 							"
+print  "									[2] Https/Http Attack 				"
 print  "									[3] Nmap Scanner 							"
 print  "									[4] Install Tools							"
 print  "									[5] Tracking 					   			"
-print  "									[6] Instagram Bros							"
+print  "									[6] Instagram Bros						"
 print
 print  " [0]> Exit "
 print
