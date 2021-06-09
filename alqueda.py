cdbc0957ebee24e5c7a27000e343f4929145c1af
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 17:24:58 -0500

    Update alqueda.py

--- alqueda.py
+++ alqueda.py
@@ -1,11 +1,11 @@
 import os, sys, time
 from time import sleep as timeout
 def restart_program():
-print  "Project Al Queda"
-print  "Created By: Rebrithed-Reaperr"
-print  "Youtube	  : BiggyGh0st"
-print  "Github    : https://github.com/Rebrithed-Reaperr"
-print  "Instagram : @pwn.doxgod"
+print "Project Al Queda"
+print "Created By: Rebrithed-Reaperr"
+print "Youtube	  : BiggyGh0st"
+print "Github    : https://github.com/Rebrithed-Reaperr"
+print "Instagram : @pwn.doxgod"
 print
 print  "	[1] DDos Attack 							"
 print  "	[2] Https/Http Attack 				"
