8fa393faa0729e4aac07397db369662018e9486f
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 17:01:44 -0500

    Add files via upload

---
+++ alqueda.py
@@ -0,0 +1,23 @@
+import os, sys, time
+from time import sleep as timeout
+def restart_program():
+	        python = sys.executable
+        os.execl(python, python, * sys.argv)
+        curdir = os.getcwd()
+os.system("clear")
+os.system("figlet Al Queda")
+print  "Created By: Rebrithed-Reaperr"
+print  "Youtube		 : BiggyGh0st"
+print  "Github    : https://github.com/Rebrithed-Reaperr"
+print  "Instagram : @pwn.doxgod"
+print
+print  "									[1] DDos Attack 								"
+print  "									[2] Https/Http Attack 		"
+print  "									[3] Nmap Scanner 							"
+print  "									[4] Install Tools							"
+print  "									[5] Tracking 					   			"
+print  "									[6] Instagram Bros						"
+print
+print  " [0]> Exit "
+print
+A = raw_input("AlQueda ==>> ")
