443b29383c70be583d6a550dcdde7750fed7126c
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 23:41:23 -0500

    Update DDoS-Attacks.py

--- DDoS-Attacks.py
+++ DDoS-Attacks.py
@@ -15,17 +15,23 @@ print
 bhydra = raw_input(" DDoS Console ==>> ")
 
 if bhydra == '01' or bhydra == '1':
+	os.system(' clear ')
 	os.system(' cd DDoS-Attack && python2 ddos-attack.py ')
 elif bhydra == '02' or bhydra == '2':
+	os.system(' clear ')
 	os.system(' sudo apt-get install python3-pycurl python3-geoip python3-whois python3-crypto python3-requests python3-scapy libgeoip1 libgeoip-dev python3 setup.py install ')
 elif bhydra == '03' or bhydra == '3':
+	os.system(' clear ')
 	os.system(' cd Raven-Storm && pip3 install -r requirements.txt && python3 main.py ')
 elif bhydra == '04' or bhydra == '4':
+	os.system(' clear ')
 	os.system(' cd CC-attack && python3 cc.py ')
 elif bhydra == '05' or bhydra == '5':
+	os.system(' clear ')
 	os.system(' sudo pip3 install colorama termcolor requests ')
 	os.system(' cd Overload-DoS && chmod +x * && ./install-overload ')
 elif bhydra == '06' or bhydra == '6':
+	os.system(' clear ')
 	os.system(' git clone https://github.com/Ha3MrX/DDos-Attack && git clone https://github.com/epsylon/ufonet && git clone https://github.com/Taguar258/Raven-Storm && git clone https://github.com/Leeon123/CC-attack && git clone https://github.com/codingplanets/Overload-DoS && python2 alqueda.py ')
 elif bhyrda == '00' or bhydra == '0':
 	sys.exit()
