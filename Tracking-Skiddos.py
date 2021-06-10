914996058424b883382586afdaa68e3755a37db5
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 20:25:29 -0500

    Add files via upload

---
+++ Tracking-Skiddos.py
@@ -0,0 +1,32 @@
+import os, sys, time
+from time import sleep as timeout
+def restart_program():
+	os.system(' clear ')
+	os.system(' figlet Tracking-Skiddos')
+print " [01]> track-ip													"
+print " [02]> ip-tracekr											"
+print " [03]> ip-tracer 										 "
+print " [04]> install 													"
+print
+print " [00]> Exit																	"
+print
+rebirthed = raw_input(" Tracking-Skiddos ==>> ")
+if rebirthed == '01' or rebirthed == '1':
+	os.system(' cd track-ip && chmod +x * && ./trackip ')
+
+elif rebirthed == '02' or rebirthed == '2':
+	os.system(' cd IP-Tracker && chmod +x * IP-Tracker.py && python3 IP-Tracker.py ')
+
+elif rebirthed == '03' or rebirthed == '3':
+	os.system(' cd IP-Tracer && chmod +x * && ./install ')
+
+elif rebirthed == '04' or rebirthed == '4':
+	os.system(' git clone https://github.com/htr-tech/track-ip && git clone https://github.com/anonymousproo/IP-Tracker && git clone https://github.com/rajkumardusad/IP-Tracer ')
+
+elif rebirthed == '00' or rebirthed == '0':
+	sys.exit()
+	
+else:
+	     print "\nERROR: Wrong Input"
+      timeout(3)
+      restart_program()
