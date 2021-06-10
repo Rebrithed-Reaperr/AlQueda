40896626f5b8a8bee46d64f73a66803dc51cfb1e
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 23:25:22 -0500

    Update Tracking-Skiddos.py

--- Tracking-Skiddos.py
+++ Tracking-Skiddos.py
@@ -2,12 +2,13 @@ import os, sys, time
 from time import sleep as timeout
 def restart_program():
 	os.system(' clear ')
-print " [01]> track-ip													"
-print " [02]> ip-tracekr											"
-print " [03]> ip-tracer 										 "
-print " [04]> install 													"
+print " [01]> track-ip		"
+print " [02]> ip-tracekr	"
+print " [03]> ip-tracer 	"
+print " [04]> install 		"
+print " [05]> Sigit 		"
 print
-print " [00]> Exit																	"
+print " [00]> Exit		"
 print
 rebirthed = raw_input(" Tracking-Skiddos ==>> ")
 if rebirthed == '01' or rebirthed == '1':
@@ -20,10 +21,12 @@ elif rebirthed == '03' or rebirthed == '3':
 	os.system(' cd IP-Tracer && chmod +x * && ./install ')
 
 elif rebirthed == '04' or rebirthed == '4':
-	os.system(' git clone https://github.com/htr-tech/track-ip && git clone https://github.com/anonymousproo/IP-Tracker && git clone https://github.com/rajkumardusad/IP-Tracer && python2 Tracking-Skiddos.py ')
+	os.system(' git clone https://github.com/termuxhackers-id/SIGIT && git clone https://github.com/htr-tech/track-ip && git clone https://github.com/anonymousproo/IP-Tracker && git clone https://github.com/rajkumardusad/IP-Tracer && python2 Tracking-Skiddos.py ')
 
 elif rebirthed == '00' or rebirthed == '0':
 	sys.exit()
+elif rebirthed == '05' or rebirthed == '5':
+	os.system(' cd SIGIT && bash install.sh && python3 sigit.py ')
 	
 else:
       print "\nERROR: Wrong Input"
