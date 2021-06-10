dd8760f563aece478de55d98d8429f059c5ec2c2
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 19:55:13 -0500

    Update alqueda.py

--- alqueda.py
+++ alqueda.py
@@ -11,26 +11,22 @@ print
 print " [1]> DDoS-Attacks            "
 print " [2]> Website-Attacks         "
 print " [3]> Tracking Skiddos        "
-print " [4]> Installing Tools        "
-print " [5]> Instagrams              "
+print " [4]> Instagrams              "
 print
 print " [0]> Exit                    "
 A = raw_input(" AlQueda ==>> ")
 
 if A == '1' or A == '01':
-  os.system(' python2 ddos-attacks.py ')
+  os.system(' python2 DDoS-Attacks.py ')
 
 elif A == '2' or A == '02':
-  os.system(' python2 website-attacks.py ')
+  os.system(' python2 Website-Attacks.py ')
 
 elif A == '3' or A == '03':
-  os.system(' python2 tracking-skiddos.py ')
+  os.system(' python2 Tracking-Skiddos.py ')
 
 elif A == '4' or A == '04':
-  os.system(' python2 install.py ')
-
-elif A == '5' or A == '05':
-  os.system(' python2 instagrams.py ')
+  os.system(' python2 Instagrams.py ')
 
 elif A == '0' or A == '00':
   sys.exit()
