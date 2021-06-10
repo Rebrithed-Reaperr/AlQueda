338d59d5dd2b4dc62fe2682086678d1ad6f11a57
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-10 00:15:38 -0500

    Update DDoS-Attacks.py

--- DDoS-Attacks.py
+++ DDoS-Attacks.py
@@ -16,7 +16,7 @@ bhydra = raw_input(" DDoS Console ==>> ")
 
 if bhydra == '01' or bhydra == '1':
 	os.system(' clear ')
-	os.system(' cd DDoS-Attack && python2 ddos-attack.py ')
+	os.system(' cd DDos-Attack && python2 ddos-attack.py ')
 elif bhydra == '02' or bhydra == '2':
 	os.system(' clear ')
 	os.system(' sudo apt-get install python3-pycurl python3-geoip python3-whois python3-crypto python3-requests python3-scapy libgeoip1 libgeoip-dev python3 setup.py install ')
@@ -32,7 +32,7 @@ elif bhydra == '05' or bhydra == '5':
 	os.system(' cd Overload-DoS && chmod +x * && ./install-overload ')
 elif bhydra == '06' or bhydra == '6':
 	os.system(' clear ')
-	os.system(' git clone https://github.com/Ha3MrX/DDos-Attack && git clone https://github.com/epsylon/ufonet && git clone https://github.com/Taguar258/Raven-Storm && git clone https://github.com/Leeon123/CC-attack && git clone https://github.com/codingplanets/Overload-DoS && python2 alqueda.py ')
+	os.system(' git clone https://github.com/Ha3MrX/DDos-Attack && git clone https://github.com/epsylon/ufonet && git clone https://github.com/Taguar258/Raven-Storm && git clone https://github.com/Leeon123/CC-attack && git clone https://github.com/codingplanets/Overload-DoS && python2 DDoS-Attacks.py')
 elif bhyrda == '00' or bhydra == '0':
 	sys.exit()
 
