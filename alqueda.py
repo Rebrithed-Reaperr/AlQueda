510ef7c028417c26772257bee8c14630894bf1f9
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 19:00:19 -0500

    Update alqueda.py

--- alqueda.py
+++ alqueda.py
@@ -1,19 +1,19 @@
 import os, sys, time
 from time import sleep as timeout
 def restart_program():
-print  "Created By: Rebrithed-Reaperr"
-print  "Youtube	  : BiggyGh0st"
-print  "Github    : https://github.com/Rebrithed-Reaperr"
-print  "Instagram : @pwn.doxgod"
+print (" Created By: Rebrithed-Reaperr ")
+print (" Youtube	  : BiggyGh0st ")
+print (" Github    : https://github.com/Rebrithed-Reaperr ")
+print (" Instagram : @pwn.doxgod ")
 print
-print  "	[1] DDos Attack 							"
-print  "	[2] Https/Http Attack 				"
-print  "	[3] Nmap Scanner 							"
-print  "	[4] Install Tools							"
-print  "	[5] Tracking 					   			"
-print  "	[6] Instagram Bros						"
+print ("	[1] DDos Attack 							")
+print ("	[2] Https/Http Attack 				")
+print ("	[3] Nmap Scanner 							")
+print ("	[4] Install Tools							")
+print ("	[5] Tracking 					   			")
+print ("	[6] Instagram Bros						")
 print
-print  " [0]> Exit "
+print (" [0]> Exit ")
 print
 A = raw_input("AlQueda ==>> ")
 
