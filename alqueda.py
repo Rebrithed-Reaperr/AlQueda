9ca94ccc5b8e4247ec2bff1a6ae0868610d14ee2
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 19:17:40 -0500

    Update alqueda.py

--- alqueda.py
+++ alqueda.py
@@ -9,10 +9,28 @@ print "Github: Rebrithed-Reaperr     "
 print "Instagram: pwn.doxgod         "
 print
 print " [1]> DDoS-Attacks            "
-print " [2]> Https/Http Attacks      "
+print " [2]> Website-Attacks         "
 print " [3]> Tracking Skiddos        "
 print " [4]> Installing Tools        "
 print " [5]> Instagrams              "
 print
 print " [0]> Exit                    "
 A = raw_input(" AlQueda ==>> ")
+
+if A == '1' or A == '01':
+  os.system(' python2 ddos-attacks.py ')
+if A == '2' or A == '02':
+  os.system(' python2 website-attacks.py ')
+if A == '3' or A == '03':
+  os.system(' python2 tracking-skiddos.py ')
+if A == '4' or A == '04':
+  os.system(' python2 install.py ')
+if A == '5' or A == '05':
+  os.system(' python2 instagrams.py ')
+if A == '0' or A == '00':
+  sys.exit()
+  
+else:
+      print "\nERROR: Wrong Input"
+      timeout(3)
+      restart_program()
