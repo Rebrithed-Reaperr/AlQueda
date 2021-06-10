dbc5e68b12c2896dfdd20f5769db3b7cebfe28e1
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 21:00:57 -0500

    Update alqueda.py

--- alqueda.py
+++ alqueda.py
@@ -2,7 +2,6 @@ import os, sys, time
 from time import sleep as timeout
 def restart_program():
   os.system(' clear ')
-  os.system(' figlet Al Queda ')
 print "                                             "
 print " ▄▄▄· ▄▄▌      .▄▄▄  ▄• ▄▌▄▄▄ .·▄▄▄▄   ▄▄▄·  "
 print "▐█ ▀█ ██•      ▐▀•▀█ █▪██▌▀▄.▀·██▪ ██ ▐█ ▀█  "
