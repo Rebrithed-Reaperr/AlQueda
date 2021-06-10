b62080c16880391d8d5e497d9d3ef9108fac5c9d
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 19:11:21 -0500

    Update alqueda.py

--- alqueda.py
+++ alqueda.py
@@ -4,3 +4,15 @@ def restart_program():
   os.system(' clear ')
   os.system(' figlet Al Queda ')
 print "Created By: Rebrithed-Reaperr "
+print "YouTube: BiggyGh0st           "
+print "Github: Rebrithed-Reaperr     "
+print "Instagram: pwn.doxgod         "
+print
+print " [1]> DDoS-Attacks            "
+print " [2]> Https/Http Attacks      "
+print " [3]> Tracking Skiddos        "
+print " [4]> Installing Tools        "
+print " [5]> Instagrams              "
+print
+print " [0]> Exit                    "
+A = raw_input(" AlQueda ==>> ")
