777b37e9bde27b60cba8d934c9e26e3cf64832af
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-09 19:03:36 -0500

    Update alqueda.py

--- alqueda.py
+++ alqueda.py
@@ -1,24 +1,5 @@
 import os, sys, time
 from time import sleep as timeout
 def restart_program():
-print (" Created By: Rebrithed-Reaperr ")
-print (" Youtube	  : BiggyGh0st ")
-print (" Github    : https://github.com/Rebrithed-Reaperr ")
-print (" Instagram : @pwn.doxgod ")
-print
-print ("	[1] DDos Attack 							")
-print ("	[2] Https/Http Attack 				")
-print ("	[3] Nmap Scanner 							")
-print ("	[4] Install Tools							")
-print ("	[5] Tracking 					   			")
-print ("	[6] Instagram Bros						")
-print
-print (" [0]> Exit ")
-print
-A = raw_input("AlQueda ==>> ")
-
-if A == "1" or A == "01":
-  os.system(' python2 ddos-attack.py ')
-  
-elif A == "2" or A == "02":
-  os.system(' python2 https/http attacks.py ')
+  os.system(' clear ')
+  os.system(' figlet Al Queda ')
