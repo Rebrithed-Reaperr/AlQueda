9669fa03cc6002c38307c32e079cfa14cd75606b
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-10 23:06:14 -0500

    Add files via upload

---
+++ Cam-Hacking.py
@@ -0,0 +1,29 @@
+import os, sys, time
+from time import sleep as timeout
+def restart_program():
+  os.system(' clear ')
+print
+print " [01]> Cam-Hackers "
+print " [02]> CamHack     "
+print " [03]> CamPhish    "
+print " [04]> Installing  "
+print
+print " [00]> EXIT 					  "
+print
+Cam = raw_input(" Cam Hacking ==>> ")
+if Cam == '01' or Cam == '1':
+	os.system(' clear ')
+	os.system(' cd Cam-Hackers && python3 cam-hackers.py ')
+elif Cam == '02' or Cam == '2':
+	os.system(' clear ')
+	os.system(' cd CamHack && chmod +x * && bash CamHack.sh ')
+elif Cam == '03' or Cam == '3':
+	os.system(' clear ')
+	os.system(' cd CanPhish && bash camphish.sh ')
+elif Cam == '04' or Cam == '4':
+	os.system(' clear ')
+	os.system(' pip3 install requests colorama php openssh wget && git clone https://github.com/aktechunt3r/CamPhish && git clone https://github.com/Devil-Tigers/CamHack && https://github.com/AngelSecurityTeam/Cam-Hackers ')
+elif Cam == '00' or Cam == '0':
+	os.system(' clear ')
+	print " Goodbye "
+	time.sleep(2)
