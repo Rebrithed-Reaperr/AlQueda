8b80974d8a9c0fdc57cc5cbc232ee6bac1e8a80f
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-11 16:47:42 -0500

    Update Wifi-Hacking.py

--- Wifi-Hacking.py
+++ Wifi-Hacking.py
@@ -1,24 +1,10 @@
 import os, sys, time
-from time import sleep as timeouut
+from time import sleep as timeout
 def restart_program():
 	os.system(' clear ')
+print " [01]> Wifi-Hacker  "
+print " [02]> Wifi-Hacking "
 print
-print " [01]> Wifi-Hacker   "
-print " [02]> Wifi-Hacking  "
-print " [03]> Installing 		 "   
+print " [00]> Exit 	   "
 print
-print " [00]> EXIT "
-print
-WifiF = raw_input(" WiFi-Hacking ==>> ")
-
-if WiFiF == '01' or WiFiF == '1':
-	os.system(' clear ')
-	os.system(' cd wifi-hacker && chmod +x wifi-hacker && ./wifi-hacker ')
-elif WiFiF == '02' or WiFiF == '2':
-	os.system(' clear ')
-	os.system(' cd Wifi-Hacking && python3 Wifi-Hacking.py ')
-elif WiFiF == '03' or WiFiF == '3':
-	os.system(' clear ')
-	os.system(' git clone https://github.com/esc0rtd3w/wifi-hacker && git clone https://github.com/ankit0183/Wifi-Hacking ')
-elif WiFiF == '00' or WiFiF == '0':
-	os.system(' python2 alqueda.py ')
+Wifi = raw_input(" Wifi Hacking ==>> ")
