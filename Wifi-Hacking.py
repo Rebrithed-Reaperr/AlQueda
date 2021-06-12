6435df1014f099060abef6d32c38b02af42671db
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-11 23:53:45 -0500

    Update Wifi-Hacking.py

--- Wifi-Hacking.py
+++ Wifi-Hacking.py
@@ -10,18 +10,18 @@ print
 print " [00]> Exit 	   "
 print
 Wifi = raw_input(" Wifi Hacking ==>> ")
-if Wifi == '[01]' or Wifi == '1':
+if Wifi == '01' or Wifi == '1':
 	os.system(' clear ')
 	os.system(' cd wifi-hacker && chmod +x * && bash wifi-hacker.sh ')
-elif Wifi == '[02]' or Wifi == '2':
+elif Wifi == '02' or Wifi == '2':
 	os.system(' clear ')
 	os.system(' cd Wifi-Hacking && python3 Wifi-Hacking.py ')
-elif Wifi == '[03]' or Wifi == '3':
+elif Wifi == '03' or Wifi == '3':
 	os.system(' clear ')
 	os.system(' cd lazyaircrack && chmod +x lazyaircrack.sh && bash lazyaircrack.sh ')
-elif Wifi == '[04]' or Wifi == '4':
+elif Wifi == '04' or Wifi == '4':
 	os.system(' clear ')
 	os.system(' sudo apt install python3 aircrack-ng && git clone https://github.com/3xploitGuy/lazyaircrack && git clone https://github.com/ankit0183/Wifi-Hacking && git clone https://github.com/esc0rtd3w/wifi-hacker ')
-elif Wifi == '[00]' or Wifi == '0':
+elif Wifi == '00' or Wifi == '0':
 	os.system(' clear ')
 	os.system(' python2 alqueda.py ')
