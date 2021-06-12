cc7f0f601f6c823128be7638610a14399dd25dd5
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-12 15:44:37 -0500

    Update Website-Attacks.py

--- Website-Attacks.py
+++ Website-Attacks.py
@@ -1,16 +1,26 @@
 import os, sys, time
 from time import sleep as timeout
 def restart_program():
-	os.system(' clear ')
+	os.system(' clear ') 
+print (" ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄        ▄▄▄▄▄▄▄▄▄▄▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄    ▄  ▄▄▄▄▄▄▄▄▄▄▄  ")
+print ("▐░▌          ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌      ▐░░░░░░░░░░░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌▐░░░░░░░░░░░▌ ")
+print ("▐░▌          ▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌       ▀▀▀▀▀▀▀▀▀█░▌          ▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌ ▐░▌ ▐░█▀▀▀▀▀▀▀▀▀  ")
+print ("▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌               ▐░▌           ▐░▌       ▐░▌     ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌▐░▌  ▐░▌           ")
+print ("▐░▌          ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄▄▄  ▐░▌▄▄▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌     ▐░▌          ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌░▌   ▐░█▄▄▄▄▄▄▄▄▄  ")
+print ("▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░▌     ▐░░░░░░░░░░░▌▐░▌          ▐░░▌    ▐░░░░░░░░░░░▌ ")
+print ("▐░▌          ▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀  ▀▀▀▀▀▀▀▀▀▀▀▐░▌  ▀▀▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌     ▐░▌          ▐░▌     ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░▌░▌    ▀▀▀▀▀▀▀▀▀█░▌ ")
+print ("▐░▌          ▐░▌       ▐░▌     ▐░▌     ▐░▌          ▐░▌     ▐░▌             ▐░▌               ▐░▌       ▐░▌     ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌▐░▌            ▐░▌ ")
+print ("▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌           ▐░▌                ▐░▌       ▐░▌     ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▌  ▄▄▄▄▄▄▄▄▄█░▌ ")
+print ("▐░░░░░░░░░░░▌▐░▌       ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌         ▐░▌                 ▐░▌       ▐░▌     ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌▐░░░░░░░░░░░▌ ")
+print (" ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀           ▀                   ▀         ▀       ▀            ▀       ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀    ▀  ▀▀▀▀▀▀▀▀▀▀▀  ")                                                                                                                                                                                  
+print (" [01]> DDoS-Ripper    ")
+print (" [02]> Layer-7-Attack ")
+print (" [03]> httpflood      ")
+print (" [04]> Install        ")
 print
-print " [01]> DDoS-Ripper    "
-print " [02]> Layer-7-Attack "
-print " [03]> httpflood      "
-print " [04]> Install        "
+print (" [00]> Exit	     ")
 print
-print " [00]> Exit											"
-print
-cyber = raw_input(" Website-Attacks ==>> ")
+cyber = input(" Website-Attacks ==>> ")
 
 if cyber == '01' or cyber == '1':
 	os.system(' clear ')
@@ -29,8 +39,8 @@ elif cyber == '04' or cyber == '4':
 	os.system(' git clone https://github.com/palahsu/DDoS-Ripper && git clone https://github.com/MusH4ck007/Layer-7-Attack && git clone https://github.com/Leeon123/golang-httpflood && python2 Website-Attacks.py ')
 
 elif cyber == '00' or cyber == '0':
-	os.system(' python2 alqueda.py ')
+	os.system(' python3 alqueda.py ')
 else:
-	print "\n[!] ERROR : Wrong Input"
+	print (" \n[!] ERROR : Wrong Input ")
 	time.sleep(1)
 	restart_program()
