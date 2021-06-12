4ae8fdd1105ace60225110b7af363701a7502eaa
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-12 17:08:36 -0500

    Update DDoS-Attacks.py

--- DDoS-Attacks.py
+++ DDoS-Attacks.py
@@ -39,7 +39,7 @@ elif bhydra == '05' or bhydra == '5':
 	os.system(' cd Overload-DoS && chmod +x * && ./install-overload ')
 elif bhydra == '06' or bhydra == '6':
 	os.system(' clear ')
-	os.system(' git clone https://github.com/Rebrithed-Reaperr/DRAPE && git clone https://github.com/Ha3MrX/DDos-Attack && git clone https://github.com/epsylon/ufonet && git clone https://github.com/Taguar258/Raven-Storm && git clone https://github.com/Leeon123/CC-attack && git clone https://github.com/codingplanets/Overload-DoS && python2 DDoS-Attacks.py')
+	os.system(' git clone https://github.com/MHProDev/MHDDoS && git clone https://github.com/Rebrithed-Reaperr/DRAPE && git clone https://github.com/Ha3MrX/DDos-Attack && git clone https://github.com/epsylon/ufonet && git clone https://github.com/Taguar258/Raven-Storm && git clone https://github.com/Leeon123/CC-attack && git clone https://github.com/codingplanets/Overload-DoS && python2 DDoS-Attacks.py')
 elif bhydra == '00' or bhydra == '0':
 	os.system(' python2 alqueda.py ')
 elif bhydra == '07' or bhydra == '7':
@@ -50,6 +50,6 @@ elif bhydra == '08' or bhydra == '8':
 	os.system(' cd MHDDoS && pip3 install -r requirements.txt && python3 start.py ')
 
 else:
-	print (" \n[!] ERROR : Wrong Input ")
+	print (" ERROR : Wrong Input ")
 	time.sleep(1)
 	restart_program()
