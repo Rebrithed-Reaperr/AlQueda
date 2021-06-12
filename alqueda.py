dda06251cc35243f0daea05b2de14cf97ef70c40
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-12 15:29:42 -0500

    Update alqueda.py

--- alqueda.py
+++ alqueda.py
@@ -3,32 +3,46 @@ from time import sleep as timeout
 def restart_program():
   os.system(' clear ')
 print
-print " Created By: Rebrithed-Reaperr "
-print " YouTube: BiggyGh0st           "
-print " Github: Rebrithed-Reaperr     "
-print " Instagram: pwn.doxgod         "
+print ("                                                                                                                           ")
+print (" ██████╗ ██████╗  ██████╗      ██╗███████╗ ██████╗████████╗     █████╗ ██╗      ██████╗ ██╗   ██╗███████╗██████╗  █████╗   ")
+print (" ██╔══██╗██╔══██╗██╔═══██╗     ██║██╔════╝██╔════╝╚══██╔══╝    ██╔══██╗██║     ██╔═══██╗██║   ██║██╔════╝██╔══██╗██╔══██╗  ")
+print (" ██████╔╝██████╔╝██║   ██║     ██║█████╗  ██║        ██║       ███████║██║     ██║   ██║██║   ██║█████╗  ██║  ██║███████║  ")
+print (" ██╔═══╝ ██╔══██╗██║   ██║██   ██║██╔══╝  ██║        ██║       ██╔══██║██║     ██║▄▄ ██║██║   ██║██╔══╝  ██║  ██║██╔══██║  ")
+print (" ██║     ██║  ██║╚██████╔╝╚█████╔╝███████╗╚██████╗   ██║       ██║  ██║███████╗╚██████╔╝╚██████╔╝███████╗██████╔╝██║  ██║  ")
+print (" ╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ╚══════╝ ╚═════╝   ╚═╝       ╚═╝  ╚═╝╚══════╝ ╚══▀▀═╝  ╚═════╝ ╚══════╝╚═════╝ ╚═╝  ╚═╝  ")
+print ("
+time.sleep(2)
+print ("▄▄▄▄·  ▄· ▄▌     ▐ ▄       • ▌ ▄ ·. ▄▄▄ .▄▄▄   ▄▄· ▪  ▪     ▄▄▄ .▐▄• ▄ ▄▄▄ . ")
+print ("▐█ ▀█▪▐█▪██▌    •█▌▐█▪     ·██ ▐███▪▀▄.▀·▀▄ █·▐█ ▌▪██ ██    ▀▄.▀· █▌█▌▪▀▄.▀· ")
+print ("▐█▀▀█▄▐█▌▐█▪    ▐█▐▐▌ ▄█▀▄ ▐█ ▌▐▌▐█·▐▀▀▪▄▐▀▀▄ ██ ▄▄▐█·▐█·   ▐▀▀▪▄ ·██· ▐▀▀▪▄ ")
+print ("██▄▪▐█ ▐█▀·.    ██▐█▌▐█▌.▐▌██ ██▌▐█▌▐█▄▄▌▐█•█▌▐███▌▐█▌▐█▌   ▐█▄▄▌▪▐█·█▌▐█▄▄▌ ")
+print ("·▀▀▀▀   ▀ •     ▀▀ █▪ ▀█▄▀▪▀▀  █▪▀▀▀ ▀▀▀ .▀  ▀·▀▀▀ ▀▀▀▀▀▀ ▀  ▀▀▀ •▀▀ ▀▀ ▀▀▀  ")
+print (" Created By: Rebrithed-Reaperr ")
+print (" YouTube: BiggyGh0st           ")
+print (" Github: Rebrithed-Reaperr     ")
+print (" Instagram: pwn.doxgod         ")
 print
-print " [1]> DDoS-Attacks            "
-print " [2]> Website-Attacks         "
-print " [3]> Tracking Skiddos        "
-print " [4]> Instagrams              "
-print " [5]> Wifi-Hacking            "
-print " [6]> Can-Hacking             "
+print (" [1]> DDoS-Attacks            ")
+print (" [2]> Website-Attacks         ")
+print (" [3]> Tracking Skiddos        ")
+print (" [4]> Instagrams              ")
+print (" [5]> Wifi-Hacking            ")
+print (" [6]> Can-Hacking             ")
 print
-print " [0]> Exit                    "
+print (" [0]> Exit                    ")
 A = raw_input(" AlQueda ==>> ")
 
 if A == '1' or A == '01':
   os.system(' clear ')
-  os.system(' python2 DDoS-Attacks.py ')
+  os.system(' python3 DDoS-Attacks.py ')
 
 elif A == '2' or A == '02':
   os.system(' clear ')
-  os.system(' python2 Website-Attacks.py ')
+  os.system(' python3 Website-Attacks.py ')
 
 elif A == '3' or A == '03':
   os.system(' clear ')
-  os.system(' python2 Tracking-Skiddos.py ')
+  os.system(' python3 Tracking-Skiddos.py ')
 
 elif A == '4' or A == '04':
  os.system(' clear ')
@@ -41,12 +55,12 @@ elif A == '0' or A == '00':
 
 elif A == '5' or A == '05':
   os.system(' clear ')
-  os.system(' python2 Wifi-Hacking.py ')
+  os.system(' python3 Wifi-Hacking.py ')
 
 elif A == '6' or A == '06':
   os.system(' clear ')
-  os.system(' python2 Cam-Hacking.py ')
+  os.system(' python3 Cam-Hacking.py ')
 else:
-      print "\nERROR: Wrong Input"
+      print ("\nERROR: Wrong Input")
       timeout(3)
       restart_program()
