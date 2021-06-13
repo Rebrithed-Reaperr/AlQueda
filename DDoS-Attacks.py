95c53fad0257c80340967372e27007c784705b48
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-13 10:32:43 -0500

    Update DDoS-Attacks.py

--- DDoS-Attacks.py
+++ DDoS-Attacks.py
@@ -35,7 +35,7 @@ elif bhydra == '04' or bhydra == '4':
 	os.system(' cd Overload-DoS && chmod +x * && ./install-overload ')
 elif bhydra == '05' or bhydra == '5':
 	os.system(' clear ')
-	os.system(' git clone https://github.com/MHProDev/MHDDoS && git clone https://github.com/Rebrithed-Reaperr/DRAPE && git clone https://github.com/Ha3MrX/DDos-Attack && git clone https://github.com/epsylon/ufonet && git clone https://github.com/Taguar258/Raven-Storm && git clone https://github.com/Leeon123/CC-attack && git clone https://github.com/codingplanets/Overload-DoS && python2 DDoS-Attacks.py')
+	os.system(' git clone https://github.com/MHProDev/MHDDoS && git clone https://github.com/Ha3MrX/DDos-Attack && git clone https://github.com/epsylon/ufonet && git clone https://github.com/Taguar258/Raven-Storm && git clone https://github.com/Leeon123/CC-attack && git clone https://github.com/codingplanets/Overload-DoS && python2 DDoS-Attacks.py')
 elif bhydra == '00' or bhydra == '0':
 	os.system(' python2 alqueda.py ')
 elif bhydra == '06' or bhydra == '6':
