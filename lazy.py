da30f007812fe9e0a1fb9c317f3855168fcfe79f
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-21 14:19:16 -0500

    Update lazy.py

--- lazy.py
+++ lazy.py
@@ -1,1 +1,13 @@
-
+import os, sys, time, colorama
+from time import sleep
+from colorama import Fore
+print (Fore.RED+" Welcome To Lazy, Happy Hacking :) ")
+time.sleep(1.5)
+os.system(' clear ')
+print (Fore.BLUE+"██╗      █████╗ ███████╗██╗   ██╗ ")
+print (Fore.BLUE+"██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝ ")
+print (Fore.BLUE+"██║     ███████║  ███╔╝  ╚████╔╝  ")
+print (Fore.BLUE+"██║     ██╔══██║ ███╔╝    ╚██╔╝   ")
+print (Fore.BLUE+"███████╗██║  ██║███████╗   ██║    ")
+print (Fore.BLUE+"╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝    ")  
+                  
